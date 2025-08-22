dee7e233c32504c78308622d2815029b2ae34fba
Zacarias Bendeck <ZacariasBendeck@users.noreply.github.com>
2025-08-22 22:16:44 +0000

    added yaml file

--- hello.py
+++ hello.py
@@ -2,5 +2,6 @@ def greet(name: str) -> str:
     return f"Hello, {name}! Welcome to GitHub Codespaces 🚀"
 
 if __name__ == "__main__":
-    # You can change the name here and rerun
-    print(greet("World"))
+    name = input("What is your name? ")
+    print(greet(name))
+
