dd86a8490a5d2493d9898760b41d2fa91424c458
Zacarias Bendeck <ZacariasBendeck@users.noreply.github.com>
2025-08-22 16:02:26 -0600

    Create hello.py
    initial basic commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+def greet(name: str) -> str:
+    return f"Hello, {name}! Welcome to GitHub Codespaces 🚀"
+
+if __name__ == "__main__":
+    # You can change the name here and rerun
+    print(greet("World"))
